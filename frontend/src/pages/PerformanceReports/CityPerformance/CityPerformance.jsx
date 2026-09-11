fba495126bfb3bca35a8a5710c522d1b4d401da8
k-asmita <khambeasmita3@gmail.com>
2026-09-11 16:48:28 +0530

    Replace useData with userow import

--- frontend/src/pages/PerformanceReports/CityPerformance/CityPerformance.jsx
+++ frontend/src/pages/PerformanceReports/CityPerformance/CityPerformance.jsx
@@ -10,7 +10,7 @@ import {
   CartesianGrid
 } from "recharts";
 
-import { useData } from "../../../context/DataContext";
+import { userow } from "../../../context/DataContext";
 
 import "./CityPerformance.css";
 
